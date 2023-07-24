784f810269e25fe57031f26dd1622fe3e9edbab7
Oscar Mesejo <oscar.mesejo@gmail.com>
2023-07-24 18:17:49 +0200

    last commit first day

--- game.js
+++ game.js
@@ -1,5 +1,15 @@
 class Game {
     constructor(){
-        
+        this.gameScreen = document.getElementById("game-creen");
+        // this.startScreen = document.getElementById("start")
+        this.width = '';
+        this.height = '';
+        this.obstacles = [];
+        this.score = 0;
+        this.timer = '';
+        this.gameOver = false;
+        // this.gameEndScreen = document
+
+
     }
 }
